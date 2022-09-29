2e50306c7958ae73e94be97e3ead116d0f98a47f
swagat <swagat@>
2022-09-29 00:14:20 -0500

    Initial commit

---
+++ app/src/main/java/com/example/weatherapp/MainActivity.java
@@ -0,0 +1,59 @@
+package com.example.weatherapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class MainActivity extends AppCompatActivity {
+EditText edit;
+TextView text;
+ String url ="https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
+ String apikey="4afa8c05ca8c27b8e2921461124691a0 ";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        edit=findViewById(R.id.edit);
+        text = findViewById(R.id.text);
+    }
+    public void getweather(View v){
+        Retrofit retrofit= new Retrofit.Builder()
+                .baseUrl("https://api.openweathermap.org/data/2.5/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        weatherapi myapi=retrofit.create(weatherapi.class);
+        Call<example> examplecall=myapi.getweather(edit.getText().toString().trim(),apikey);
+        examplecall.enqueue(new Callback<com.example.weatherapp.example>() {
+            @Override
+            public void onResponse(Call<com.example.weatherapp.example> call, Response<com.example.weatherapp.example> response) {
+             if(response.code()==404){
+                 Toast.makeText(MainActivity.this,"Please enter a valid city",Toast.LENGTH_LONG).show();
+             }else if(!(response.isSuccessful() )){
+                 Toast.makeText(MainActivity.this,response.code(),Toast.LENGTH_LONG).show();
+             }
+             example mydata=response.body();
+             Main main=mydata.getMain();
+             Double temp=main.getTemp();
+             Integer temperature=(int)(temp-273.15);
+             text.setText(String.valueOf(temperature)+"C");
+            }
+
+            @Override
+            public void onFailure(Call<com.example.weatherapp.example> call, Throwable t) {
+                Toast.makeText(MainActivity.this,t.getMessage(),Toast.LENGTH_LONG).show();
+
+            }
+        });
+
+    }
+}
---
+++ app/src/main/java/com/example/weatherapp/example.java
@@ -0,0 +1,13 @@
+package com.example.weatherapp;
+
+public class example {
+    Main main;
+
+    public Main getMain() {
+        return main;
+    }
+
+    public void setMain(Main main) {
+        this.main = main;
+    }
+}
